f7ebfea3895a0427c96f8def5e878d4774160974
Terry Higbee <talanzzz@yahoo.com>
2019-11-18 07:07:14 -0700

    more of the kivy demo working

--- gui/main.js
+++ gui/main.js
@@ -17,7 +17,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('gui.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
